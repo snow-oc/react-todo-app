9bf93712d67c94dfda3ca6a63fba55c165c5914a
snow-oc <snow7349.oc@gmail.com>
2026-02-22 18:49:54 +0900

    タスクの削除機能実装

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -17,6 +17,12 @@ export const Todo = () => {
     setTodoText("");
   };
 
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+  }
+
   return(
     <>
       <div className="input-area">
@@ -26,12 +32,12 @@ export const Todo = () => {
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          {incompleteTodos.map((todo) => (
+          {incompleteTodos.map((todo, index) => (
             <li key={todo}>
               <div className="list-row">
                 <p className="todo-item">{todo}</p>
                 <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             </li>
           ))}
