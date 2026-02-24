691792bc79f90af3d24049459fd6caaa77c485d4
snow-oc <snow7349.oc@gmail.com>
2026-02-25 00:05:35 +0900

    リファクタ　コンポーネント内でのスタイル設定

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,7 +1,16 @@
 export const InputTodo = (props) => {
   const { todoText, onChange, onClick } = props;
+  const style = {
+    backgroundColor: "#c6e5d9",
+    width: "400px",
+    height: "30px",
+    padding: "8px",
+    margin: "8px",
+    borderRadius: "8px"
+  }
+
   return (
-    <div className="input-area">
+    <div style={style}>
       <input type="text" placeholder="TODOを入力" value={todoText} onChange={onChange} />
       <button onClick={onClick}>追加</button>
     </div>
